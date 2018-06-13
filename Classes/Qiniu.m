c14e5b635af6913cbc186ad3d24e8ba7c6a78e79
frank_chen <792151095@qq.com>
2018-06-13 16:58:36 +0800

    初始化项目

---
+++ Classes/Qiniu.m
@@ -0,0 +1,20 @@
+//
+//  QBMUtil.m
+//  WeexEros
+//
+//  Created by qbm_ios on 2018/5/24.
+//  Copyright © 2018年 qbm Company. All rights reserved.
+
+#import "QBMUtil.h"
+#import <WeexPluginLoader/WeexPluginLoader.h>
+
+WX_PlUGIN_EXPORT_MODULE(qiniu, QBMDatePickerModule)
+@implementation Qiniu
+WX_EXPORT_METHOD(@selector(log))
+
++ (void)log
+{
+    NSLog(@"====>测试");
+}
+@end
+
